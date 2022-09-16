3077fb212bbbf6d948242afa51a8ba9cbae6574f
Marcel <marcelclavero94@gmail.com>
2022-09-16 21:47:08 +0200

    add project with e2e tests

---
+++ cypress/e2e/login.spec.ts
@@ -0,0 +1,66 @@
+describe('Login specs', () => {
+  it('visit the login page', () => {
+    // Arrange
+
+    // Act
+    cy.visit('/');
+
+    // Assert
+  });
+
+  it('should name input has the focus when it clicks on it', () => {
+    // Arrange
+
+    // Act
+    cy.visit('/');
+    cy.findByRole('textbox').click();
+
+    // Assert
+    cy.findByRole('textbox').should('have.focus');
+  });
+
+  it('should show an alert with a message when type invalid credentials', () => {
+    // Arrange
+    const user = 'admin';
+    const password = '1234'; // bad password
+    cy.on('window:alert', cy.stub().as('alertStub'));
+
+    // Act
+    cy.visit('/');
+    cy.findByLabelText('Name').as('userInput');
+    cy.findByLabelText('Password').as('passwordInput');
+    cy.findByRole('button', { name: 'Login' }).as('loginButton');
+
+    cy.get('@userInput').type(user);
+    cy.get('@passwordInput').type(password);
+    cy.get('@loginButton').click();
+
+    // Assert
+    cy.get('@userInput').should('have.value', user);
+    cy.get('@passwordInput').should('have.value', password);
+    cy.get('@alertStub').should(
+      'have.been.calledWith',
+      'invalid credentials, use admin/test, excercise: display a mui snackbar instead of this alert.'
+    );
+  });
+
+  it('should navigate to hotels url when type valid credentials', () => {
+    // Arrange
+    const user = 'admin';
+    const password = 'test'; // bad password
+    cy.on('window:alert', cy.stub().as('alertStub'));
+
+    // Act
+    cy.visit('/');
+    cy.findByLabelText('Name').as('userInput');
+    cy.findByLabelText('Password').as('passwordInput');
+    cy.findByRole('button', { name: 'Login' }).as('loginButton');
+
+    cy.get('@userInput').type(user);
+    cy.get('@passwordInput').type(password);
+    cy.get('@loginButton').click();
+
+    // Assert
+    cy.url().should('equal', 'http://localhost:8080/#/hotel-collection');
+  });
+});
